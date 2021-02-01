2c42c4c7d3b8285af07fbd291864f8ac67d812fb
SliferDMC <marodriguezn@uqvirtual.edu.co>
2021-02-01 15:59:17 -0500

    Clasificación de los estudiantes: Desertores, Ausentes y Activos

--- logic.py
+++ logic.py
@@ -4,7 +4,7 @@ import xlrd
 import json
 from statistics import mode
 
-filePath = "C:\\Users\\migue\\OneDrive\\Escritorio\\Proyecto de Grado\\Base_datos_Copia.xls"
+filePath = "C:\\Users\\migue\\OneDrive\\Escritorio\\BD\\Base_datos_Copia.xls"
 openFile = xlrd.open_workbook(filePath)
 
 indexSheets = ["2020_02", "2020_01", "2019_02", "2019_01", "2018_02", "2018_01", "2017_02", "2017_01"]
@@ -76,6 +76,51 @@ def complete_void(array, v):
             array[i] = v
     return array
 
+# Determina si un valor se encuentra contenido en una lista
+def is_content(v, array):
+    try:
+        array.index(v)
+        return True
+    except ValueError:
+        return False
+
+# Realiza la caracterizacion de los estudiantes: Activos, Ausentes y Desertores
+# Parametros: documents: listado de documentos del año analizado
+#             labels: registros de caracterización del año anterior
+def do_characterization(labels, documents):
+    result = [[],[]]
+    for i in range(len(labels[0])):
+        status = labels[0][i]
+        doc = labels[1][i]
+        if status == 'ACTIVO' or status == 'NUEVO':
+            if is_content(doc, documents):
+                result[0].append('ACTIVO')
+                result[1].append(doc)
+            else:
+                result[0].append('AUSENTE')
+                result[1].append(doc)
+        elif status == 'AUSENTE':
+            if is_content(doc, documents):
+                result[0].append('ACTIVO')
+                result[1].append(doc)
+            else:
+                result[0].append('DESERTOR')
+                result[1].append(doc)
+        elif status == 'DESERTOR' or status == 'DESERTOR-A':
+            if is_content(doc, documents):
+                result[0].append('ACTIVO')
+                result[1].append(doc)
+            else:
+                result[0].append('DESERTOR-A')
+                result[1].append(doc)
+    for doc in documents:
+        if not is_content(doc, labels[1]):
+            result[0].append('NUEVO')
+            result[1].append(doc)
+    return result
+
+#############################################################################
+
 # Carga de los datos de la BD y conversion a un vector de DataFrame
 for i in range(len(indexSheets)):
     sheets.append(openFile.sheet_by_name(indexSheets[i]))
@@ -130,6 +175,65 @@ for p in range(len(arrays)):
 
 dfs = convert_npArrays_to_DataFrames(arrays)
 
+# Determinar estudiantes desertores de la universidad con respecto al año
+### Año 2017_2
+labels_2017_2 = [[],[]]
+
+documents_2017_1 = arrays[7][1:,0]
+documents_2017_2 = arrays[6][1:,0]
+coincidences1 = np.intersect1d(documents_2017_1, documents_2017_2).tolist()
+
+for doc in documents_2017_2:
+    if is_content(doc, coincidences1):
+        labels_2017_2[0].append('ACTIVO')
+        labels_2017_2[1].append(doc)
+    else:
+        labels_2017_2[0].append('NUEVO')
+        labels_2017_2[1].append(doc)
+
+for doc in documents_2017_1:
+    if not is_content(doc, coincidences1):
+        labels_2017_2[0].append('AUSENTE')
+        labels_2017_2[1].append(doc)
+
+### Año 2018_1
+documents_2018_1 = arrays[5][1:,0].tolist()
+labels_2018_1 = do_characterization(labels_2017_2, documents_2018_1)
+
+### Año 2018_2
+documents_2018_2 = arrays[4][1:,0].tolist()
+labels_2018_2 = do_characterization(labels_2018_1, documents_2018_2)
+
+### Año 2019_1
+documents_2019_1 = arrays[3][1:,0].tolist()
+labels_2019_1 = do_characterization(labels_2018_2, documents_2019_1)
+
+### Año 2019_2
+documents_2019_2 = arrays[2][1:,0].tolist()
+labels_2019_2 = do_characterization(labels_2019_1, documents_2019_2)
+
+### Año 2020_1
+documents_2020_1 = arrays[1][1:,0].tolist()
+labels_2020_1 = do_characterization(labels_2019_2, documents_2020_1)
+
+### Año 2020_2
+documents_2020_2 = arrays[0][1:,0].tolist()
+labels_2020_2 = do_characterization(labels_2020_1, documents_2020_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -142,6 +246,82 @@ dfs = convert_npArrays_to_DataFrames(arrays)
 
 
 """
+print('labels_2017_1')
+print('Cantidad de estudiantes: ', arrays[7].shape)
+print('\n')
+
+print('labels_2017_2')
+print('Cantidad de estudiantes: ', arrays[6].shape)
+print('ACTIVOS: ', labels_2017_2[0].count('ACTIVO'))
+print('AUSENTES: ', labels_2017_2[0].count('AUSENTE'))
+print('DESERTORES: ', labels_2017_2[0].count('DESERTOR'))
+print('NUEVOS: ', labels_2017_2[0].count('NUEVO'))
+print('DESERTORES ANTIGUOS: ', labels_2017_2[0].count('DESERTOR-A'))
+print('\n')
+
+print('labels_2018_1')
+print('Cantidad de estudiantes: ', arrays[5].shape)
+print('ACTIVOS: ', labels_2018_1[0].count('ACTIVO'))
+print('AUSENTES: ', labels_2018_1[0].count('AUSENTE'))
+print('DESERTORES: ', labels_2018_1[0].count('DESERTOR'))
+print('NUEVOS: ', labels_2018_1[0].count('NUEVO'))
+print('DESERTORES ANTIGUOS: ', labels_2018_1[0].count('DESERTOR-A'))
+print('\n')
+
+print('labels_2018_2')
+print('Cantidad de estudiantes: ', arrays[4].shape)
+print('ACTIVOS: ', labels_2018_2[0].count('ACTIVO'))
+print('AUSENTES: ', labels_2018_2[0].count('AUSENTE'))
+print('DESERTORES: ', labels_2018_2[0].count('DESERTOR'))
+print('NUEVOS: ', labels_2018_2[0].count('NUEVO'))
+print('DESERTORES ANTIGUOS: ', labels_2018_2[0].count('DESERTOR-A'))
+print('\n')
+
+print('labels_2019_1')
+print('Cantidad de estudiantes: ', arrays[3].shape)
+print('ACTIVOS: ', labels_2019_1[0].count('ACTIVO'))
+print('AUSENTES: ', labels_2019_1[0].count('AUSENTE'))
+print('DESERTORES: ', labels_2019_1[0].count('DESERTOR'))
+print('NUEVOS: ', labels_2019_1[0].count('NUEVO'))
+print('DESERTORES ANTIGUOS: ', labels_2019_1[0].count('DESERTOR-A'))
+print('\n')
+
+print('labels_2019_2')
+print('Cantidad de estudiantes: ', arrays[2].shape)
+print('ACTIVOS: ', labels_2019_2[0].count('ACTIVO'))
+print('AUSENTES: ', labels_2019_2[0].count('AUSENTE'))
+print('DESERTORES: ', labels_2019_2[0].count('DESERTOR'))
+print('NUEVOS: ', labels_2019_2[0].count('NUEVO'))
+print('DESERTORES ANTIGUOS: ', labels_2019_2[0].count('DESERTOR-A'))
+print('\n')
+
+print('labels_2020_1')
+print('Cantidad de estudiantes: ', arrays[1].shape)
+print('ACTIVOS: ', labels_2020_1[0].count('ACTIVO'))
+print('AUSENTES: ', labels_2020_1[0].count('AUSENTE'))
+print('DESERTORES: ', labels_2020_1[0].count('DESERTOR'))
+print('NUEVOS: ', labels_2020_1[0].count('NUEVO'))
+print('DESERTORES ANTIGUOS: ', labels_2020_1[0].count('DESERTOR-A'))
+print('\n')
+
+print('labels_2020_2')
+print('Cantidad de estudiantes: ', arrays[0].shape)
+print('ACTIVOS: ', labels_2020_2[0].count('ACTIVO'))
+print('AUSENTES: ', labels_2020_2[0].count('AUSENTE'))
+print('DESERTORES: ', labels_2020_2[0].count('DESERTOR'))
+print('NUEVOS: ', labels_2020_2[0].count('NUEVO'))
+print('DESERTORES ANTIGUOS: ', labels_2020_2[0].count('DESERTOR-A'))
+print('\n')
+
+
+
+
+
+
+
+
+
+
 cont1 = 0
 for array in arrays:
     for j in range(len(array[0])):
--- pruebas.py
+++ pruebas.py
@@ -2,47 +2,16 @@ import numpy as np
 import pandas as pd
 from statistics import mode
 
-def is_Number(value):
-    try:
-        int(value)
-        return True
-    except ValueError:
-        return False
-
-def calculate_average(array):
-    avr = 0
-    cont = 0
-    for i in array:
-        if is_Number(i):
-            avr += float(i)
-            cont += 1
-    return avr/cont
-
-def vector_is_number_or_string(array):
-    for i in array:
-        if is_Number(i):
-            return True
-    return False
-
-def complete_void(array, v):
-    for i in range(len(array)):
-        if array[i] == '-':
-            array[i] = v
-    return array
-
-
-
-array = np.array([['Numero','vacio','palabras'],[1,2,3],[4,'-',6],[7,'hola','hola']])
-array2 = np.array([['3,2', 'hola'],['2,3','8']])
+l = [1,2,3,4,5,5,5,4]
+array = np.array([1,2,3])
+array2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
 df = pd.DataFrame(array)
+a = np.intersect1d(array, array2).tolist()
 
-a = '2'
-
-b = float(a)
-
-print(b)
-
-
+print(array2.shape)
+print(l.count(1))
+print(l.count(4))
+print(l.count(6))
 
 
 
@@ -59,6 +28,8 @@ print(b)
 
 
 """
+a.append(1)
+
 print(df)
 print(df.shape)
 
