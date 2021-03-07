618bd28cdf092ee2a2ec1dd96b231bfe0fcd85e1
SliferDMC <marodriguezn@uqvirtual.edu.co>
2021-03-06 20:46:57 -0500

    Nueva clase graphic.py

---
+++ graphic.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import xlrd
+
+R1 = np.loadtxt('../BD/R1.txt', skiprows=0, dtype=str)
+R2 = np.loadtxt('../BD/R2.txt', skiprows=0, dtype=str)
+R3 = np.loadtxt('../BD/R3.txt', skiprows=0, dtype=str)
+R4 = np.loadtxt('../BD/R4.txt', skiprows=0, dtype=str)
+R5 = np.loadtxt('../BD/R5.txt', skiprows=0, dtype=str)
+R6 = np.loadtxt('../BD/R6.txt', skiprows=0, dtype=str)
+R7 = np.loadtxt('../BD/R7.txt', skiprows=0, dtype=str)
+
+BD0 = np.loadtxt('../BD/BD0.txt', skiprows=0, dtype=str)
+BD1 = np.loadtxt('../BD/BD1.txt', skiprows=0, dtype=str)
+BD2 = np.loadtxt('../BD/BD2.txt', skiprows=0, dtype=str)
+BD3 = np.loadtxt('../BD/BD3.txt', skiprows=0, dtype=str)
+BD4 = np.loadtxt('../BD/BD4.txt', skiprows=0, dtype=str)
+BD5 = np.loadtxt('../BD/BD5.txt', skiprows=0, dtype=str)
+BD6 = np.loadtxt('../BD/BD6.txt', skiprows=0, dtype=str)
+BD7 = np.loadtxt('../BD/BD7.txt', skiprows=0, dtype=str)
